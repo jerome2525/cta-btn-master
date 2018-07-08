8258ecb9ff98bb569aeac826762167c1ba6f84fd
Jerome2525 <emorej.enela@gmail.com>
2018-07-08 23:08:22 +0800

    1.0 Plugin info fixed

--- cta-btn.php
+++ cta-btn.php
@@ -4,11 +4,9 @@ Plugin Name: CTA Button
 Description: To use CTA button to any part of the pages with a nice cool admin
 Plugin URI: http://wp-needs.com
 Author: Jerome Anyayahan
-Author URI: http://wp-needs.com
 Version: 1.0
 License: GPL2
-Text Domain: Text Domain
-Domain Path: Domain Path
+Text Domain: cta-btn
 */
 
 require plugin_dir_path( __FILE__ ) . 'inc/cta-btn.php';
